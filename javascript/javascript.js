aca9f98fa9a5d5763a5bd08fbc5662b6d2b65f2b
Chi Ho <chiho_33@hotmail.com>
2018-01-18 20:54:37 +0100

    fixed button fetch api

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,19 @@
 (function() {
 	"use strict";
+
+	const getJokesButton = document.getElementById('getData');
+	getJokesButton.addEventListener('click', getData);
+	
+	// fetch data from api
+	function getData() {
+		fetch('http://api.icndb.com/jokes/random/10')
+			.then(function(res) {
+				return res.json()
+			}).then(function(data) {
+				console.log(data);
+			}).catch(function(err) {
+				console.log(err)
+			})
+		}
 })();
 

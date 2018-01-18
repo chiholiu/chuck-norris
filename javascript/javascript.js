898da1ea2c279eb847bcab6389b671e783a138b1
Chi Ho <chiho_33@hotmail.com>
2018-01-18 21:50:32 +0100

    fixed typeError data.forEach

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,10 +10,29 @@
 			.then(function(res) {
 				return res.json()
 			}).then(function(data) {
-				console.log(data);
+				console.log(data.value);
 			}).catch(function(err) {
 				console.log(err)
 			})
+		
+
+		fetch('http://api.icndb.com/jokes/random/10')
+            .then((res) => { return res.json() })
+            .then((data) => { 
+            	console.log(data.value);
+                let result = '<h2> Chuck Norris Jokes </h2>';
+                data.value.forEach((joke) => {
+                    result +=
+                     `<h4> User ID: ${joke.id} </h4>
+                     <ul>
+                       <li> User title : ${joke.joke}</li>
+                    </ul>
+                     `;
+                document.getElementById('list-of-jokes').innerHTML = result;
+                });
+           }).catch((err)=>console.log(err))
+
 		}
+
 })();
 

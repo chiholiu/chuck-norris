f3e5cb89d9c14949a4f046e3cf87ddbdcca8ba21
Chi Ho <chiho_33@hotmail.com>
2018-01-19 19:05:52 +0100

    bind input checkboxes when html is generated

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,91 +18,46 @@
 		fetch('https://api.icndb.com/jokes/random/10')
 		.then(res => res.json() )
 		.then(data => { 
-
-			console.log(data);
 			let result;
 			data.value.forEach((joke) => {
 				result +=
 				`<li><input type="checkbox"> User title : ${joke.joke}</li>
 				`;
 				document.getElementById('list-of-jokes').innerHTML = result;
+				
 			})
+			bindCheckbox();
 		}).catch((err)=>console.log(err))
 	}
 
-	let defaultList = document.getElementById('list-of-jokes');
-	let favorite = document.getElementById('favorites');
+	function bindCheckbox() {
+		let defaultList = document.getElementById('list-of-jokes');
+		let favorite = document.getElementById('favorites');
 
-	
+		let $elems = $(defaultList).children();
+		console.log($elems.length);
+		if( $elems.length > 0) {
+			console.log('loaded');
+			$("input[type=checkbox]").change(function () {
+	    	
+	    	let $this = $(this),
+	        i = $this.closest('li').index(),
+	        $target = $(favorite);
 
-	var completeTask = function(){
-  
-	  //GRAB THE CHECKBOX'S PARENT ELEMENT, THE LI IT'S IN
-	  var listItem = this.parentNode;
-	  console.log(listItem);
-	  
-	  //CREATE AND INSERT THE DELETE BUTTON
-	  var deleteBtn = document.createElement("button"); // <button>
-	  deleteBtn.innerText ="Delete"; 
-	  deleteBtn.className = "delete";
-	  listItem.appendChild(deleteBtn);
-	  
-	  //SELECT THE CHECKBOX FROM THE COMPLETED CHECKBOX AND REMOVE IT
-	  var checkBox = listItem.querySelector("input[type=checkbox]");
-	  checkBox.remove();
-	  
-	  //PLACE IT INSIDE THE COMPLETED LIST
-	  favorite.appendChild(listItem); 
-	  
-	  //BIND THE NEW COMPLETED LIST
-	  bindCompleteItems(listItem, deleteTask);
-	};
+	        if (!this.checked) {
+	            $target = $(defaultList);
+	        } else {
+	            $this.data('idx', i);
+	        }
 
-
-	var deleteTask = function(){
-	  console.log("Deleting task...");
-	  
-	  var listItem = this.parentNode;
-	  var ul = listItem.parentNode;
-	  
-	  ul.removeChild(listItem);
-	  
-	};
-
-	var bindIncompleteItems = function(taskItem, checkBoxClick){  
-	  console.log("Binding the incomplete list...");
-	  
-	  //BIND THE CHECKBOX TO A VAR
-	  var checkBox = taskItem.querySelector("input[type=checkbox]");
-	  
-	  //SETUP EVENT LISTENER FOR THE CHECKBOX
-	  checkBox.onchange = checkBoxClick;  
-	}; 
-
-	//A FUNCTIONM THAT BINDS EACH OF THE ELEMTS IN THE COMPLETE LIST
-	var bindCompleteItems = function(taskItem, deleteButtonPress){
-	  console.log("Binding the complete list...");
-	  
-	  //BIND THE DELETE BUTTON
-	  var deleteButton = taskItem.querySelector(".delete");
-	   
-	  //WHEN THE DELETE BUTTIN IS PRESSED, RUN THE deleteTask function
-	  deleteButton.onclick = deleteButtonPress;
-	    
-	};
-
-	var listItem = document.createElement("li");
-
-	bindIncompleteItems(listItem, completeTask);
-
-
-for(var i=0; i < defaultList.children.length; i++) {
-  bindIncompleteItems(defaultList.children[i], completeTask);
-}
-
-for(var i=0; i < favorite.children.length; i++) {
-  bindCompleteItems(favorite.children[i], deleteTask);
-}
-
+	        let $targetLi = $target.find('li:eq(' + $this.data('idx') + ')');
+	        if ($targetLi.length) {
+	            $target.find('li:eq(' + $this.data('idx') + ')').before($this.closest('li'));
+	        } else {
+	            $target.append($this.closest('li'));
+	        }
+		});
+		}
+	}
 })();
 

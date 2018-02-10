a418a5868e1664ed9347b396b8fcad32614f4f6e
Chi Ho Liu <chiholiu@chis-air.home>
2018-02-10 22:57:28 +0100

    changed entire structure

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,44 +1,40 @@
  (function() {
 	"use strict";
-		
+
 	const getJokesButton = document.getElementById('getData');
 	getJokesButton.addEventListener('click', getData);
+	let favoriteJokes = document.getElementById('favorites');
+	let listOfJokes = document.getElementById("list-of-jokes"); 
 
-	loadLocalStorage();
+	// add to empty array before it will be pushed into localStorage
+	let favorite = [];
 
-	function loadLocalStorage() {
-		let storage = JSON.parse(localStorage.getItem('favoList')) || [];
-		let listOfFavorites = document.getElementById("favorites");
-		let emptyArray = '';
-		if(storage.length > 0) {
-			for(let i = 0; i < storage.length; i++) {
-				let idNumberJoke = storage[i].id;
-				emptyArray += 
-				`<li><input type="checkbox" id='${idNumberJoke}'/> User title: ${storage[i].joke}</li>`;
-				listOfFavorites.innerHTML = emptyArray;
-			}
-		} else {
-			return false;
+	function displayFavorites() {
+		let favoriteArray = listFavorite();
+		let listOfFavorites = document.getElementById('favorites');
+		let output = "";
+		for (let i in favoriteArray) {
+			output += `<li><input type="checkbox" id=${favoriteArray[i].jokeId}'/> User title: ${favoriteArray[i].jokeText}<input type="button" class="delete" value="Click Me"></li>`;
 		}
+		listOfFavorites.innerHTML = output;
+		bindCheckBoxFavJoke(listOfFavorites.children);
 	}
-	
-	// fetch data from api
-	function getData() {
-		let listOfJokes = document.getElementById("list-of-jokes"); 
 
+    // fetch data from api
+	function getData() {
 		fetch('https://api.icndb.com/jokes/random/10')
-		.then(function(res) {
-			return res.json();
+			.then(function(res) {
+				return res.json();
 		}).then(function(data) { 
 			// variable is undefined because it is not initialized. Therefore at some empty single quotes
 			let result = '';
 			console.log(data.value);
-			data.value.forEach((joke) => {
+			data.value.forEach((joke, index) => {
 				result +=
-				`<li><input type="checkbox" class='inputCheckbox' id='${joke.id}'/> User title :  ${joke.joke}</li>`;
+				`<li><input type="checkbox" class='inputCheckbox' id='${joke.id}'/> User title :  ${joke.joke}<input type="button" class="delete" value="Click Me"></li>`;
 				listOfJokes.innerHTML = result;
 			});
-			bindCheckbox();
+			bindCheckboxJoke(listOfJokes.children);
 		}).catch(function(err) {
 			console.log(err);
 		});
@@ -49,62 +45,105 @@
 		getJokesButton.classList.add('opacity');
 	}
 
-	function bindCheckbox() {
-		let inputCheckbox = document.querySelectorAll('input[type=checkbox]');
-		let elems = document.getElementById('list-of-jokes').childNodes;
-		let favoriteList = document.getElementById('favorites');
-		let fav = JSON.parse(localStorage.getItem('favoList'))|| [];
-
-		if(elems.length > 0) {	
-			inputCheckbox.forEach(function(element, index) {
-				inputCheckbox[index].addEventListener('change', function() {
-					let joke = this;
-					if(joke.checked && joke.parentNode.parentNode.id === 'list-of-jokes') { 
-					   joke.checked = false;
-					   favoriteList.appendChild(joke.parentNode);
-					   addFavorite(joke.id, joke.parentNode.innerText, fav);
-					} 
-					if(joke.checked && joke.parentNode.parentNode.id === 'favorites') {
-					   joke.checked = false;
-					   removeFavorite(joke, index);
-					}
-				});
-			});
+	function bindCheckboxJoke(listOfJokes) {
+		console.log(listOfJokes.length);
+		for(let i = 0; i < listOfJokes.length; i++) {
+			bindEventListeners(listOfJokes[i], addFavorite);
 		}
-		clickedButton();
 	}
 
-	function removeFavorite(favorite, index) {
-		let favoriteCheckBox = favorite;
-		let i = index;
+	function bindCheckBoxFavJoke(listOfFavorites) {
+		for (let i = 0; favoriteJokes.length; i++) {
+			bindEventListeners(favoriteJokes[i], removeFavorite);
+		}
+	}
+	
+	let bindEventListeners = function(jokeItem, checkBoxEventHandler) {
+		let checkbox = jokeItem.querySelector('input[type="checkbox"]');
+		let deleteButton = jokeItem.querySelector('.delete');
+		
+		checkbox.onchange = addFavorite;
+		deleteButton.onchange = console.log('hello');
 
-		// convert iterable object to an array, otherwise splice metho d would give an error.
-		let favoriteListItem = Array.from(favoriteCheckBox.parentNode); 
-		favoriteListItem.splice(i, 1);
-		document.getElementById('list-of-jokes').appendChild(favorite.parentNode);
-		localStorage.setItem('favoList', JSON.stringify(favoriteListItem));
 	}
 
-	// store favorites in localStorage
-	function addFavorite(jokeId, jokeText, fav) {
-		let norrisJoke = {
-			id: jokeId,
-			joke: jokeText
-		};
-		let favorites = fav;
+	let addFavorite = function() {
+		let id = this.id;
+		let joke = this.parentNode.innerText;
+		this.disabled = true;
+		addJokeToFavorite(id, joke);
+	}
 
-		// which is necessarily to remove the first empty array by using the reduce function
-		const favIds = favorites.reduce((sum, element) => {
-			return sum.concat(element.id);
-			},
-		[]);
+	let removeFavorite = function() {
 
-		if(favorites.length < 10 && !(favIds.includes(jokeId))) {
-			favorites.push(norrisJoke);
-			console.log(favorites.length);
-		} 
+		let id = this.parentNode;
+		let inputButton = this.parentNode.firstChild;
+		inputButton.checked = false;
+		inputButton.disabled = false;
+		removeJokeFromFavorite(id);
+	}
 
-		// always get the object before the push method and pass it into stringify
-		localStorage.setItem('favoList', JSON.stringify(favorites));
-	}	
+	let Item = function(jokeId, jokeText) {
+		this.jokeId = jokeId
+		this.jokeText = jokeText
+	};
+
+	function addJokeToFavorite(id, joke) {
+		// check duplicates by iterating 
+		for (let i in favorite) {
+			if(favorite[i].id === id || favorite.length > 10) {
+				console.log('stop');
+				break;
+			}
+		}
+
+		let norrisJoke = new Item(id, joke);
+
+		if(favorite == null) {
+    		favorite = [];
+		}
+
+		favorite.push(norrisJoke);
+		saveFavorite();
+		displayFavorites();
+	}
+
+	function removeJokeFromFavorite(id) {
+		for (let i in favorite) {
+			if(favorite[i].id === id) {
+				favorite.splice(i, 1);
+				break;
+			}
+		}
+		saveFavorite();
+		displayFavorites();
+	}
+
+	function listFavorite() {
+		let favoriteCopy = [];
+		for (let i in favorite) {
+			let jokes = favorite[i];
+			let jokesCopy = {};
+
+			// using copy will avoid any strange error like accidentially overwriting some array, so therefore never use references in this case
+			for (let p in jokes) {
+				jokesCopy[p] = jokes[p];
+			}
+			favoriteCopy.push(jokesCopy);
+		}
+		return favoriteCopy;
+	}
+
+	function saveFavorite() {
+		localStorage.setItem('favoList', JSON.stringify(favorite));
+	}
+
+	function loadFavorites() {
+		favorite = JSON.parse(localStorage.getItem('favoList'));
+	}
+
+	// You have to load the JSOn of course before 
+	loadFavorites();
+	displayFavorites();
+	
 })();
